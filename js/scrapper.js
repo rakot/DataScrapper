37da7f4c65e58468c4f7cac81c70d01c5fb09419
rakot <rakot.ss@gmail.com>
2015-12-29 02:39:32 +0300

    list add

--- js/scrapper.js
+++ js/scrapper.js
@@ -1,5 +1,37 @@
 $(function(){
-    $( document ).on( 'click', '.selectors .green', function(){
-        $('.selectors tbody').append('<tr><td><input class="u-full-width" type="text"></td><td><input class="u-full-width" type="text"></td><td><button class="red" title="Delete row">-</button></td></tr>');
+    chrome.storage.sync.get('formulas', function(data) {
+        var formulas = data.formulas || [];
+
+        formulas.forEach(function(value){
+            $('#formulaList').append($('<option>').text(value.title));
+        });
+
+        $( document ).on( 'click', '.selectors .green', function(){
+            $('.selectors tbody').append('<tr><td><input class="u-full-width" type="text"></td><td><input class="u-full-width" type="text"></td><td><button class="red" title="Delete row">-</button></td></tr>');
+        });
+
+        var SetFormulaInput = $('#SetFormulaInput');
+
+        $('#AddFormula').click(function(){
+            SetFormulaInput.val('');
+            $('.overlay').show();
+        });
+
+        $('#SetFormulaName').click(function(){
+            if(SetFormulaInput.val()) {
+                formulas.push({
+                    title: SetFormulaInput.val(),
+                    row: '',
+                    rows: []
+                });
+                chrome.storage.sync.set({'formulas': formulas});
+                SetFormulaInput.val('');
+                $('.overlay').hide();
+            }
+        });
+
+        $('#CancelFormulaName').click(function(){
+            $('.overlay').hide();
+        });
     });
 });
