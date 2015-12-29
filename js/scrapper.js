1302af06dd504bdec092991d3b1e0222c90295dd
rakot <rakot.ss@gmail.com>
2015-12-29 04:32:13 +0300

    for firefox migrated from extension storage to html5 storage

--- js/scrapper.js
+++ js/scrapper.js
@@ -1,80 +1,91 @@
 $(function(){
-    chrome.storage.sync.get('formulas', function(data) {
-        var formulas = data.formulas || [];
+    var formulas = [];
+    if(localStorage.getItem('formulas')) {
+        formulas = JSON.parse(localStorage.getItem('formulas'));
+    }
 
-        var changeCurrentFormula = function() {
-            if(formulas.length) {
-                var formula = formulas[$('#formulaList').val()];
-                if(formula) {
-                    $('#rowSelector').val(formula.row);
-                    $('.selectors tbody tr').remove();
-                    formula.rows.forEach(function(val){
-                        var tr = $('<tr><td><input class="u-full-width" type="text"></td><td><input class="u-full-width" type="text"></td><td><button class="red" title="Delete row">-</button></td></tr>');
-                        tr.find('input:eq(0)').val(val.title);
-                        tr.find('input:eq(1)').val(val.selector);
-                        $('.selectors tbody').append(tr);
-                    });
-                }
+    var saveFormulas = function() {
+        localStorage.setItem('formulas', JSON.stringify(formulas));
+    };
+
+    var changeCurrentFormula = function() {
+        if(formulas.length) {
+            var formula = formulas[$('#formulaList').val()];
+            if(formula) {
+                $('#rowSelector').val(formula.row);
+                $('.selectors tbody tr').remove();
+                formula.rows.forEach(function(val){
+                    var tr = $('<tr><td><input class="u-full-width" type="text"></td><td><input class="u-full-width" type="text"></td><td><button class="red" title="Delete row">-</button></td></tr>');
+                    tr.find('input:eq(0)').val(val.title);
+                    tr.find('input:eq(1)').val(val.selector);
+                    $('.selectors tbody').append(tr);
+                });
             }
-        };
+        }
+    };
 
-        var saveCurrentFormula = function() {
-            if(formulas.length) {
-                var formula = formulas[$('#formulaList').val()];
-                if(formula) {
-                    formula.row = $('#rowSelector').val();
-                    formula.rows = [];
-                    $('.selectors tbody tr').each(function(){
-                        var self = $(this);
-                        var title = self.find('input:eq(0)').val();
-                        var selector = self.find('input:eq(1)').val();
-                        if(selector) {
-                            formula.rows.push({
-                                title: title,
-                                selector: selector
-                            });
-                        }
-                    });
-                }
+    var saveCurrentFormula = function() {
+        if(formulas.length) {
+            var formula = formulas[$('#formulaList').val()];
+            if(formula) {
+                formula.row = $('#rowSelector').val();
+                formula.rows = [];
+                $('.selectors tbody tr').each(function(){
+                    var self = $(this);
+                    var title = self.find('input:eq(0)').val();
+                    var selector = self.find('input:eq(1)').val();
+                    if(selector) {
+                        formula.rows.push({
+                            title: title,
+                            selector: selector
+                        });
+                    }
+                });
             }
-            chrome.storage.sync.set({'formulas': formulas});
-        };
+        }
+        saveFormulas();
+    };
 
+    var updateFormulaList = function() {
         var i = 0;
         formulas.forEach(function(value){
             $('#formulaList').append($('<option>').val(i++).text(value.title));
         });
-        changeCurrentFormula();
-        $('#formulaList').change(changeCurrentFormula);
+    };
+    updateFormulaList();
+    changeCurrentFormula();
+    $('#formulaList').change(changeCurrentFormula);
 
-        $( document ).on( 'click', '.selectors .green', function(){
-            $('.selectors tbody').append('<tr><td><input class="u-full-width" type="text"></td><td><input class="u-full-width" type="text"></td><td><button class="red" title="Delete row">-</button></td></tr>');
-        });
-
-        var SetFormulaInput = $('#SetFormulaInput');
-
-        $('#AddFormula').click(function(){
-            SetFormulaInput.val('');
-            $('.overlay').show();
-        });
-
-        $('#SetFormulaName').click(function(){
-            if(SetFormulaInput.val()) {
-                formulas.push({
-                    title: SetFormulaInput.val(),
-                    row: '',
-                    rows: []
-                });
-                chrome.storage.sync.set({'formulas': formulas});
-                SetFormulaInput.val('');
-                $('.overlay').hide();
-            }
-        });
-
-        $('#CancelFormulaName').click(function(){
-            $('.overlay').hide();
-        });
-
-        $('#SaveButton').click(saveCurrentFormula);
+    $( document ).on( 'click', '.selectors .green', function(){
+        $('.selectors tbody').append('<tr><td><input class="u-full-width" type="text"></td><td><input class="u-full-width" type="text"></td><td><button class="red" title="Delete row">-</button></td></tr>');
     });
+
+    var SetFormulaInput = $('#SetFormulaInput');
+
+    $('#AddFormula').click(function(){
+        SetFormulaInput.val('');
+        $('.overlay').show();
+    });
+
+    $('#SetFormulaName').click(function(){
+        if(SetFormulaInput.val()) {
+            formulas.push({
+                title: SetFormulaInput.val(),
+                row: '',
+                rows: []
+            });
+            saveFormulas();
+            SetFormulaInput.val('');
+            $('.overlay').hide();
+            updateFormulaList();
+            $('#formulaList').val($('#formulaList option:last').val());
+            changeCurrentFormula();
+        }
+    });
+
+    $('#CancelFormulaName').click(function(){
+        $('.overlay').hide();
+    });
+
+    $('#SaveButton').click(saveCurrentFormula);
 });
--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,3 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
